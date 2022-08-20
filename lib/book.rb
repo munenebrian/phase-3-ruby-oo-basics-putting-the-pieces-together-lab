79befc6a6dfdb1ff1a69c20c0ab14dca5411927d
munenebrian <njerubrianmunene@gmail.com>
2022-08-20 16:29:27 +0300

    commit tree 4d18cbc8fafdc912b13619e9dac1c9e93256de2b

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,16 @@
+class Book
+  attr_accessor :title, :author, :genre, :page_count
 
+  def initialize(title)
+      @title = title
+  end
+
+  def turn_page
+     puts "Flipping the page...wow, you read fast!"
+  end
+end
+
+car = Book.new('Sleek')
+puts car.turn_page
+puts car.title
 
